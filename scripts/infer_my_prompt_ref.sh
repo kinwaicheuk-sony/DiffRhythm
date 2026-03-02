30ac45da35a7e5a63af73345a64d7c48ca1a8af0
Kin Wai Cheuk <kinwai.cheuk@sony.com>
2026-03-02 22:33:45 +0900

    updated repo

--- scripts/infer_my_prompt_ref.sh
+++ scripts/infer_my_prompt_ref.sh
@@ -1,14 +1,5 @@
-cd "$(dirname "$0")"
-cd ../
-
-export PYTHONPATH=$PYTHONPATH:$PWD
-
-if [[ "$OSTYPE" =~ ^darwin ]]; then
-    export PHONEMIZER_ESPEAK_LIBRARY=/opt/homebrew/Cellar/espeak-ng/1.52.0/lib/libespeak-ng.dylib
-fi
-
 python3 infer/infer.py \
-    --lrc-path infer/example/my_lyrics.lrc \
+    --lrc-path infer/example/my_lyrics_no_time.lrc \
     --ref-prompt "Exciting, Energetic, Christmas song" \
     --audio-length 130 \
     --output-dir infer/example/output \
